1c078bb45a25f2f0759414934159d2c7b93a6ac7
Geming Zhu <zgming20@163.com>
2023-06-17 19:55:56 -0400

    art
    art

--- Mining/Assets/Scripts/menu.cs
+++ Mining/Assets/Scripts/menu.cs
@@ -2,9 +2,11 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.SceneManagement;
+using LootLocker.Requests;
 
 public class menu : MonoBehaviour
 {
+    public LeaderBoard leaderboard;
     // Start is called before the first frame update
     void Start()
     {
@@ -19,5 +21,17 @@ public class menu : MonoBehaviour
     public void gameStart()
     {
         SceneManager.LoadScene("GamePlay");
+    }
+    public void gameQuit()
+    {
+        Application.Quit();
+    }
+    public void gameRank()
+    {
+        StartCoroutine(loadGlobalLeaderBoard());
+    }
+    private IEnumerator loadGlobalLeaderBoard()
+    {
+        yield return leaderboard.FetchTopHighscoreRoutine();
     }
 }
